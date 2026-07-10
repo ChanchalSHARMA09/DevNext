8f5a3c1f5acda772cd82db4c98eb6ad57f9c40e9
ChanchalSHARMA09 <chanchal.2312051@gmail.com>
2026-07-10 20:59:57 +0530

    Minor Code Bug Fix

--- backend/src/utils/email.util.js
+++ backend/src/utils/email.util.js
@@ -1,11 +1,12 @@
 // src/utils/email.util.js
 import nodemailer from 'nodemailer';
+const port = parseInt(process.env.SMTP_PORT, 10),
 
 export const sendEmail = async ({ to, subject, text, html }) => {
     // Configure this with your SMTP provider (e.g., SendGrid, AWS SES, or Gmail for testing)
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
-        port: parseInt(process.env.SMTP_PORT, 10),
+        port: port,
         secure: port === 465,
         auth: {
             user: process.env.SMTP_USER,
